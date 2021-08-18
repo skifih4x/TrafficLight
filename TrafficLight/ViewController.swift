dc25eaeb7ab32f4801b6ce065d57ad879bb4c1b0
artem <arte@kb.ri>
2021-08-19 02:01:18 +0300

    didn't

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -11,16 +11,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var trafficLightRedView: UIView!
     @IBOutlet weak var trafficLightYellowView: UIView!
     @IBOutlet weak var trafficLightGreenView: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
+    
+    let notNormal: CGFloat = 0.3
+    var count = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        trafficLightRedView.alpha = 0.3
+        trafficLightRedView.layer.cornerRadius = 58
+        trafficLightYellowView.layer.cornerRadius = 58
+        trafficLightGreenView.layer.cornerRadius = 58
+        startButton.layer.cornerRadius = 10
     }
+    
     @IBAction func startButtonActive() {
+        switch count {
+        case 0:
+            trafficLightRedView.alpha = notNormal
+        case 1:
+            trafficLightYellowView.alpha = notNormal
+        case 2:
+            trafficLightGreenView.alpha = notNormal
+        default:
+            break
+        }
     }
-    
-    
-
 }
-
